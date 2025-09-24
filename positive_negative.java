db2eb0ef88e63e8ab7d1dd4d5ea93a83a29ff4a5
Krishma sri <sriduraivr@gmail.com>
2025-09-24 11:36:37 +0530

    Add files via upload

---
+++ positive_negative.java
@@ -0,0 +1,16 @@
+import java.util.*;
+  class positive_negative{
+    public static void main(String[] args) {
+        Scanner Scan=new Scanner(System.in);
+        int num=Scan.nextInt();
+        if(num>0);{
+            System.out.println(num +" is positive");
+        }
+        else if(num<0){
+            System.out.println(num +" is negative");
+        }
+            else{
+                System.err.println(num +" ");
+            } 
+    }
+    }
